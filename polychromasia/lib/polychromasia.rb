4c150d5542ca1e269286b8dd483b9d5235dd9b70
Anis Zarei <azarei@gettyimages.com>
2012-11-01 11:26:42 -0600

    Cleaning up gem

--- polychromasia/lib/polychromasia.rb
+++ polychromasia/lib/polychromasia.rb
@@ -1,5 +1,30 @@
-require "polychromasia/version"
+require 'color'
 
 module Polychromasia
-  # Your code goes here...
+	class ColorPicker
+		def random_colors_to_hex
+			color_values = Hash.new
+			color_inputs = ['red', 'green', 'blue']
+
+			color_inputs.each do |i|
+				v = rand(255)
+				color_values[i.to_s] = v
+			end
+
+			@hex_color = ""
+			color = color_values.values
+
+			color.each do |component|
+				hex = component.to_s(16)
+				if component < 10
+					@hex_color << "0#{hex}"
+				else
+					@hex_color << hex
+				end
+			end
+
+			puts @hex_color
+			return @hex_color
+		end		
+	end
 end
--- polychromasia/lib/polychromasia/version.rb
+++ polychromasia/lib/polychromasia/version.rb
@@ -1,3 +1,3 @@
 module Polychromasia
-  VERSION = "0.0.1"
+  VERSION = "0.0.3"
 end
